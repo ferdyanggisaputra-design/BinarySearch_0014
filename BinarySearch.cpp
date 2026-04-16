cc44e56718a0ced7fa92ed8a0f35356e9cc71d25
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-16 15:42:41 +0700

    BubbleSortArray

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -87,7 +87,7 @@ void binarySearch()
         {
             int mid = (low + high) / 2;
 
-            if (elemen[mid] == x)
+            if (element[mid] == x)
             {
                 cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
                 return;
@@ -112,5 +112,10 @@ void binarySearch()
 
     } while (ulang == 'y' || ulang == 'Y');
 }
-        
+
+int main()
+{
+    input();
+    bubbleSortArray
+}
     
